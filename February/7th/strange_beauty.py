74198cdd4da52cffca7b3cc61cf58fb928c66251
Dong W Kim <dongwkim@Dongs-MacBook-Air-2.local>
2021-02-06 22:37:11 -0800

    not done

--- February/7th/strange_beauty.py
+++ February/7th/strange_beauty.py
@@ -1,25 +1,25 @@
 import math
-from collections import Counter
-
+import Counter
+ 
 def strange_beauty(lst):
+    # d = Counter(lst)
 
-    d = Counter(lst)
-    lst = list(set(lst))
     lcm = lst[0]
     result = 0
-    print(lst)
     for i in range(1,len(lst)):
         new_lcm = int(lcm * lst[i]/math.gcd(lcm,lst[i]))
-        print('lcm', new_lcm)
-        if lcm != new_lcm:
-            result += d[lst[i]]
+ 
+        if new_lcm == lst[i]:
+            None
+        elif lcm != new_lcm:
+            # result += d[lst[i]]
+            result += 1
         lcm = new_lcm
-        print(result)
     return result
-
-
-
-
+ 
+ 
+ 
+ 
 n = int(input())
 for i in range(n):
     input1 = int(input())
